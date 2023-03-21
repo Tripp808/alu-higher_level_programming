efeb5d8cd1bacd7187975ca06cf45a5fbf928bc5
Tripp808 <o.ankeli@alustudent.com>
2023-03-21 10:06:24 -0700

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool" + ", BestSchool" * (magic_string.count - 1)
+    if not hasattr(magic_string, "count"):
+        magic_string.count = 0
+    magic_string.count += 1
+    return "Holberton, " * (magic_string.count - 1) + "Holberton"
