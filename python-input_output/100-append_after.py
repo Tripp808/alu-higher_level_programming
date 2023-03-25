aafdce1d8073a016931521aa16ac64e09ad7da00
Tripp808 <o.ankeli@alustudent.com>
2023-03-25 14:12:08 -0700

    100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+"""Module that inserts a line of text to a file, after each line containing a specific string"""
 
 def append_after(filename="", search_string="", new_string=""):
-    """
-    Inserts a line of text to a file, after each line containing a specific string.
-
-    Args:
-        filename (str): Name of the file
-        search_string (str): String to search for in each line of the file
-        new_string (str): String to append after each line containing the search string
-    """
-    with open(filename, mode='r+', encoding='utf-8') as f:
+    """Function that inserts a line of text to a file, after each line containing a specific string"""
+    with open(filename, mode='r', encoding='utf-8') as f:
         lines = f.readlines()
-        f.seek(0)
+
+    with open(filename, mode='w', encoding='utf-8') as f:
         for line in lines:
             f.write(line)
             if search_string in line:
                 f.write(new_string)
-        f.truncate()
+
+if __name__ == '__main__':
+    # Testing the function
+    append_after("c", "Python is cool!\n", "file = c is fun!\n")
+    append_after("nop", "Python is cool!\n", "file = Nop, nothing here Really nothing\n")
+    append_after("c_is_fun", "Python is cool!\n", "file = Here c is fun Holberton School c is fun c is fun We are Holberton c is fun\n")
+    append_after("big_text", "Python", "file = Big text\n")
+    append_after("non_existent_file", "Python", "file = Non-existent file\n")
