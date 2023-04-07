c7c1feebfcd1338134e9bfd59ff8d00659d7e46b
Tripp808 <o.ankeli@alustudent.com>
2023-04-07 19:35:23 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""I document you"""
-
 
 import urllib.request
-
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
+        utf8_content = content.decode("utf-8")
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- utf8 content: {}".format(utf8_content))
