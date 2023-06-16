9aa823455974309c6396a320b5972aa6ab66a06d
Tripp808 <o.ankeli@alustudent.com>
2023-06-15 18:01:40 -0700

    cool changes

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Script that prints 3 lines C is fun,Python is cool and JavaScript is amazing.
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
